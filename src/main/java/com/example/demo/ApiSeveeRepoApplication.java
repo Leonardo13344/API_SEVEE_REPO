1fff6d3c5ef8335514910cfaef5fc80a01b8b0d7
Nicolas Carrera <nacarrera@uce.edu.ec>
2023-01-28 22:19:44 -0500

    Map de Provincias y Cantones

--- src/main/java/com/example/demo/ApiSeveeRepoApplication.java
+++ src/main/java/com/example/demo/ApiSeveeRepoApplication.java
@@ -3,14 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.sevee.repository.modelo.Candidato;
-
 @SpringBootApplication
 public class ApiSeveeRepoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSeveeRepoApplication.class, args);
-		
 	}
 	
 
